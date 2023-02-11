63e26dd7860e376d714f16f2f009703c5d48c40b
Shilpi <svvar31@gmail.com>
2023-02-11 19:22:55 +0530

    Second Commit

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -6,6 +6,14 @@ public class Vehicle {
     private int currentSpeed;
     private int currentDirection;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getCurrentDirection() {
         return currentDirection;
     }
